8972e51e3eb3945e82415430901f80d0c097dea4
Jonny <jmwright@scottlogic.com>
2019-12-06 14:11:01 +0000

    Day3b

--- src/day3/wireCrosser.spec.ts
+++ src/day3/wireCrosser.spec.ts
@@ -1,4 +1,7 @@
-import { getClosestCrossingPoint } from './wireCrosser'
+import {
+  getClosestCrossingPoint,
+  getQuickestCrossingPoint
+} from './wireCrosser'
 
 describe('wire crosser', () => {
   it('matches the simplest example', () => {
@@ -23,5 +26,31 @@ describe('wire crosser', () => {
     )
 
     expect(actual).toBe(135)
+  })
+})
+
+describe('wire timer', () => {
+  it('matches the simplest example', () => {
+    const actual = getQuickestCrossingPoint('R8,U5,L5,D3', 'U7,R6,D4,L4')
+
+    expect(actual).toBe(30)
+  })
+
+  it('matches example 1', () => {
+    const actual = getQuickestCrossingPoint(
+      'R75,D30,R83,U83,L12,D49,R71,U7,L72',
+      'U62,R66,U55,R34,D71,R55,D58,R83'
+    )
+
+    expect(actual).toBe(610)
+  })
+
+  it('matches example 2', () => {
+    const actual = getQuickestCrossingPoint(
+      'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+      'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
+    )
+
+    expect(actual).toBe(410)
   })
 })
--- src/day3/wireCrosser.ts
+++ src/day3/wireCrosser.ts
@@ -1,11 +1,16 @@
+import e = require('express')
+
 const buildPath = (wirePath: string): Path[] => {
   let x = 0,
-    y = 0
+    y = 0,
+    length = 0
   return wirePath.split(',').map(command => {
     const currX = x
     const currY = y
+    const currLength = length
 
     const distance = Number(command.slice(1))
+    length += distance
     switch (command[0]) {
       case 'U':
         y += distance
@@ -22,7 +27,14 @@ const buildPath = (wirePath: string): Path[] => {
       default:
         break
     }
-    return { currX, currY, nextX: x, nextY: y }
+    return {
+      currX,
+      currY,
+      currLength,
+      nextX: x,
+      nextY: y,
+      nextLength: length
+    }
   })
 }
 
@@ -31,6 +43,7 @@ interface Path {
   currY: number
   nextX: number
   nextY: number
+  currLength: number
 }
 
 function* getCrossingPoints(path1: Path[], path2: Path[]) {
@@ -46,15 +59,28 @@ function* getCrossingPoints(path1: Path[], path2: Path[]) {
         path1Segment.nextY > path2Segment.nextY
 
       if (xCross && yCross) {
-        yield {
-          x:
-            path1Segment.currX === path1Segment.nextX
-              ? path1Segment.currX
-              : path2Segment.currX,
-          y:
-            path1Segment.currY === path1Segment.nextY
-              ? path1Segment.currY
-              : path2Segment.currY
+        if (path1Segment.currX === path1Segment.nextX) {
+          yield {
+            x: path1Segment.currX,
+            y: path2Segment.currY,
+            path1Distance:
+              path1Segment.currLength +
+              Math.abs(path1Segment.currY - path2Segment.currY),
+            path2Distance:
+              path2Segment.currLength +
+              Math.abs(path1Segment.currX - path2Segment.currX)
+          }
+        } else {
+          yield {
+            x: path2Segment.currX,
+            y: path1Segment.currY,
+            path1Distance:
+              path1Segment.currLength +
+              Math.abs(path1Segment.currX - path2Segment.currX),
+            path2Distance:
+              path2Segment.currLength +
+              Math.abs(path1Segment.currY - path2Segment.currY)
+          }
         }
       }
     }
@@ -68,5 +94,15 @@ export const getClosestCrossingPoint = (wire1: string, wire2: string) => {
   const crossingPoints = Array.from(getCrossingPoints(wire1Path, wire2Path))
   return crossingPoints
     .map(coord => Math.abs(coord.x) + Math.abs(coord.y))
+    .sort((a, b) => a - b)[0]
+}
+
+export const getQuickestCrossingPoint = (wire1: string, wire2: string) => {
+  const wire1Path = buildPath(wire1)
+  const wire2Path = buildPath(wire2)
+
+  const crossingPoints = Array.from(getCrossingPoints(wire1Path, wire2Path))
+  return crossingPoints
+    .map(coord => coord.path1Distance + coord.path2Distance)
     .sort((a, b) => a - b)[0]
 }
