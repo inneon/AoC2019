019e2dbfd7ea35e5e4fcdec14894970cd698ad05
Jonny <jmwright@scottlogic.com>
2020-01-09 13:20:05 +0000

    tree search

--- src/day18/keyCollector.spec.ts
+++ src/day18/keyCollector.spec.ts
@@ -1,4 +1,11 @@
-import { getStart, distances, getAllKeys, allKeys } from './keyCollector'
+import {
+  getCoord,
+  getAllKeys,
+  allKeys,
+  getAdjacentItems,
+  buildAdjacencyMap,
+  shouldExpand
+} from './keyCollector'
 
 describe('key collector', () => {
   it('can find the entrance', () => {
@@ -6,7 +13,7 @@ describe('key collector', () => {
 #b.A.@.a#
 #########`
 
-    expect(getStart(map)).toEqual({ x: 5, y: 1 })
+    expect(getCoord(map, '@')).toEqual({ x: 5, y: 1 })
   })
 
   it('can get a list of all keys', () => {
@@ -18,36 +25,71 @@ describe('key collector', () => {
     expect(allKeys(map)).toContain('b')
   })
 
-  it('can find the distance to the nearest key', () => {
+  it('can get the list of object and distances from the entrance', () => {
     const map = `#########
 #b.A.@.a#
 #########`
 
-    expect(distances(map, 5, 1, [])[0]).toEqual({
-      key: 'a',
-      dist: 2,
-      x: 7,
-      y: 1
+    expect(getAdjacentItems('@', map)).toContainEqual({
+      to: 'a',
+      dist: 2
+    })
+    expect(getAdjacentItems('@', map)).toContainEqual({
+      to: 'A',
+      dist: 2
     })
   })
 
-  it('can find the distance to keys passed a door', () => {
+  it('can build an adjacency map', () => {
     const map = `#########
-#b.A.@.a#
+#b.A..@a#
 #########`
 
-    expect(distances(map, 5, 1, ['a'])).toContainEqual({
-      key: 'b',
-      dist: 4,
-      x: 1,
-      y: 1
+    const adjacency = buildAdjacencyMap(map)
+    expect(adjacency).toEqual({
+      a: [{ to: '@', dist: 1 }],
+      A: [
+        { to: 'b', dist: 2 },
+        { to: '@', dist: 3 }
+      ],
+      b: [{ to: 'A', dist: 2 }],
+      '@': [
+        { to: 'a', dist: 1 },
+        { to: 'A', dist: 3 }
+      ]
     })
   })
 
-  it('can get all keys', () => {
+  it('should expand a node with a key', () => {
+    expect(shouldExpand('a', [], {}, 0)).toBe(true)
+  })
+
+  it('should expand a node with a open door', () => {
+    expect(shouldExpand('A', ['a'], {}, 0)).toBe(true)
+  })
+
+  it('should not expand a node with a locked door', () => {
+    expect(shouldExpand('A', [], {}, 0)).toBe(false)
+  })
+
+  it('should not expand a node that it has already explored more cheaply', () => {
+    expect(shouldExpand('a', [], { a: 2 }, 3)).toBe(false)
+  })
+
+  it.skip('can get all keys', () => {
     const map = `#########
 #b.A.@.a#
 #########`
     expect(getAllKeys(map)).toBe(8)
+  })
+
+  it.skip('agrees with example 2', () => {
+    const map = `########################
+#f.D.E.e.C.b.A.@.a.B.c.#
+######################.#
+#d.....................#
+########################`
+
+    expect(getAllKeys(map)).toBe(86)
   })
 })
--- src/day18/keyCollector.ts
+++ src/day18/keyCollector.ts
@@ -7,17 +7,24 @@ interface Coord {
 interface Space extends Coord {
   token: string
 }
+interface Adjacency {
+  dist: number
+  to: string
+}
+interface Dictionary<TValue> {
+  [key: string]: TValue
+}
 
 const toTokenisedMap = (map: string): Space[][] =>
   map
     .split('\n')
     .map((line, y) => line.split('').map((token, x) => ({ token, x, y })))
 
-export const getStart = (map: string | Space[][]) => {
+export const getCoord = (map: string | Space[][], toFind: string) => {
   if (typeof map === 'string') map = toTokenisedMap(map)
 
   const { x, y } = arrayFlatten(map).filter(
-    ({ token }: Space) => token === '@'
+    ({ token }: Space) => token === toFind
   )[0]
   return { x, y }
 }
@@ -30,19 +37,23 @@ export const allKeys = (map: string | Space[][]): string[] => {
     .map(({ token }) => token)
 }
 
-export const distances = (
-  map: string | Space[][],
-  x: number,
-  y: number,
-  gotKeys: string[]
-) => {
+const allDoors = (map: string | Space[][]): string[] => {
   if (typeof map === 'string') map = toTokenisedMap(map)
+
+  return arrayFlatten(map)
+    .filter(({ token }: Space) => 'A' <= token && token <= 'Z')
+    .map(({ token }) => token)
+}
+
+export const getAdjacentItems = (from: string, map: string | Space[][]) => {
+  if (typeof map === 'string') map = toTokenisedMap(map)
+
+  const { x, y } = getCoord(map, from)
 
   const workingCopy = map.map(line => line.map(({ token }) => token))
   let distance = 0
-  const passableSpaces = ['.', '@', ...gotKeys.map(key => key.toUpperCase())]
 
-  const keys: { dist: number; key: string; x: number; y: number }[] = []
+  const keys: Adjacency[] = []
 
   const toExplore: (Coord | 'increment')[] = [{ x, y }, 'increment']
   const enqueue = ({ x, y }) => {
@@ -57,15 +68,14 @@ export const distances = (
   }
   const exploreSpace = ({ x, y }) => {
     const current = workingCopy[y][x]
-    if ('a' <= current && current <= 'z') {
-      if (gotKeys.indexOf(current) === -1) {
-        keys.push({ dist: distance, key: current, x, y })
-      }
+    if (current === '#') {
+      // do nothing
+    } else if (current === '.' || current === from) {
       enqueue({ x, y })
-    } else if (passableSpaces.indexOf(current) !== -1) {
-      enqueue({ x, y })
+    } else {
+      keys.push({ dist: distance, to: current })
     }
-    workingCopy[y][x] = distance.toString()
+    workingCopy[y][x] = '#'
   }
 
   while (toExplore.length > 1) {
@@ -81,21 +91,87 @@ export const distances = (
   return keys
 }
 
-export const getAllKeys = (map: string | Space[][]) => {
+export const buildAdjacencyMap = (
+  map: string | Space[][]
+): Dictionary<Adjacency[]> => {
   if (typeof map === 'string') map = toTokenisedMap(map)
 
-  let total = 0
-  const gotKeys = []
-  const remaining = allKeys(map)
-  let { x, y } = getStart(map)
-  while (remaining.length > 0) {
-    const dist = distances(map, x, y, gotKeys)[0]
-    gotKeys.push(dist.key)
-    remaining.splice(remaining.indexOf(dist.key), 1)
-    total += dist.dist
-    x = dist.x
-    y = dist.y
-  }
+  return [...allKeys(map), ...allDoors(map), '@'].reduce(
+    (prev: object, token: string) => {
+      let result = {
+        ...prev
+      }
+      result[token] = getAdjacentItems(token, map)
+      return result
+    },
+    {}
+  )
+}
 
-  return total
+const isKey = (toCheck: string) => toCheck !== toCheck.toUpperCase()
+const isDoor = (toCheck: string) => toCheck !== toCheck.toLowerCase()
+
+export const shouldExpand = (
+  token: string,
+  keys: string[],
+  visited: Dictionary<number>,
+  distance: number
+) => {
+  if (isDoor(token)) {
+    return keys.indexOf(token.toLowerCase()) !== -1
+  }
+  if (!visited[token]) return true
+
+  return distance < visited[token]
+}
+
+const getAllKeysRecursively = (
+  adjencies: Dictionary<Adjacency[]>,
+  toGet: string[],
+  visited: string[],
+  keys: string[],
+  at: string,
+  current: number,
+  depth: number
+) => {
+  if (toGet.length === 0) {
+    return current
+  }
+  const dist = adjencies[at]
+  return (
+    dist
+      // .filter(({ dist, to }) => shouldExpand(to, keys))
+      .reduce((prev, { dist, to }) => {
+        if (current + dist > prev) {
+          return prev
+        }
+        if (isKey(at)) {
+          keys = [...keys, at]
+        }
+        const subDistance = getAllKeysRecursively(
+          adjencies,
+          toGet.filter(k => k !== to),
+          [...visited, to],
+          keys,
+          to,
+          current + dist,
+          depth + 1
+        )
+        return subDistance < prev ? subDistance : prev
+      }, Number.MAX_SAFE_INTEGER)
+  )
+}
+
+export const getAllKeys = (map: string | Space[][]) => {
+  if (typeof map === 'string') map = toTokenisedMap(map)
+  const remaining = allKeys(map)
+  return getAllKeysRecursively(
+    buildAdjacencyMap(map),
+    remaining,
+    [],
+    [],
+    '@',
+    0,
+    0
+  )
 }
